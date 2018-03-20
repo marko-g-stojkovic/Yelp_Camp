dc3b63b95bc7407b5314d668c1490c1a30e21c5a
Marko Stojkovic <markoottawa@gmail.com>
2018-03-20 03:38:57 +0000

    add mongolab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
     
-mongoose.connect("mongodb://localhost/yelp_camp_v11");
+// mongoose.connect("mongodb://localhost/yelp_camp_v11");
+mongoose.connect("mongodb://marko:king@ds117739.mlab.com:17739/yelpcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
